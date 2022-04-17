4ad8762078a949c5309b3b099fb8ac563351d799
Marcos Eduardo Mori Kaminski <marcosemk1005@gmail.com>
2022-04-17 20:06:24 -0300

    v1.0

--- js/app.js
+++ js/app.js
@@ -1,44 +1,31 @@
 /*** Navbar animation ***/
 const icon = document.querySelector('.nav-menu-icon');
+const menuText = document.querySelector("#menu-text");
+
+const lineTop = document.getElementById('line-top');
+const lineCenter = document.getElementById('line-center');
+const lineBottom = document.getElementById('line-bottom');
+
 const menu = document.querySelector('.nav-menu');
 const navLinks = document.querySelectorAll('.nav-menu li');
 
 icon.addEventListener('click', () =>
 {
-  menu.classList.toggle("visible");
+  /* Show menu */
   menu.classList.toggle("invisible");
 
+  /* Rotate button lines */
+  lineTop.classList.toggle('line-top-change');
+  lineCenter.classList.toggle('line-center-change');
+  lineBottom.classList.toggle('line-bottom-change');
+
+  /* Navigation buttons animation */
   navLinks.forEach((link, index) =>
   {
     if (link.style.animation) {link.style.animation = '';}
     else {link.style.animation = `nav-link 0.15s ease forwards ${index / 50 + 0.15}s`;}
   });
 })
-
-/*********************************************************************************************/
-/*** Favicon switch ***/
-matcher = window.matchMedia('(prefers-color-scheme: dark)');
-matcher.addListener(onUpdate);
-
-lightThemeIcon = document.querySelector('link#light-scheme-icon');
-darkThemeIcon = document.querySelector('link#dark-scheme-icon');
-
-function onUpdate()
-{
-  if (matcher.matches)
-  {
-    lightThemeIcon.remove();
-    document.head.append(darkThemeIcon);
-  }
-  
-  else
-  {
-    document.head.append(lightThemeIcon);
-    darkThemeIcon.remove();
-  }
-}
-
-onUpdate();
 
 /*********************************************************************************************/
 /*** News slider ***/
