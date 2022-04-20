20b9074a9f303c0d6174a96b4f4a1e8a385932a0
Marcos Eduardo Mori Kaminski <marcosemk1005@gmail.com>
2022-04-19 21:03:33 -0300

    v1.0

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-/*** Navbar animation ***/
-const icon = document.querySelector('.nav-menu-icon');
-const menuText = document.querySelector("#menu-text");
-
-const lineTop = document.getElementById('line-top');
-const lineCenter = document.getElementById('line-center');
-const lineBottom = document.getElementById('line-bottom');
-
-const menu = document.querySelector('.nav-menu');
-const navLinks = document.querySelectorAll('.nav-menu li');
-
-icon.addEventListener('click', () =>
-{
-  /* Show menu */
-  menu.classList.toggle("invisible");
-
-  /* Rotate button lines */
-  lineTop.classList.toggle('line-top-change');
-  lineCenter.classList.toggle('line-center-change');
-  lineBottom.classList.toggle('line-bottom-change');
-
-  /* Navigation buttons animation */
-  navLinks.forEach((link, index) =>
-  {
-    if (link.style.animation) {link.style.animation = '';}
-    else {link.style.animation = `nav-link 0.15s ease forwards ${index / 50 + 0.15}s`;}
-  });
-})
-
-/*********************************************************************************************/
 /*** News slider ***/
 const sliderContainer = document.querySelector(".news-slider-content");
 const arrowLeft = document.getElementsByClassName('arrow-left');
